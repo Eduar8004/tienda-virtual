ed7a258ee1846fbfb8638d86534bc937ec89ea55
eduar.ramirez <eduar.ramirez@dcsas.com.co>
2025-05-22 10:47:18 -0500

    Cliente navegacion inicio

--- app/src/main/java/com/example/tiendavirtual/MainActivity.kt
+++ app/src/main/java/com/example/tiendavirtual/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tiendavirtual.Cliente
+package com.example.tiendavirtual
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,19 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.tiendavirtual.R
 import com.example.tiendavirtual.vendedor.MainActivityVendedor
 
-class MainActivityCliente : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         screenSplash.setKeepOnScreenCondition { true }
         val intent = Intent(this, MainActivityVendedor::class.java)
         startActivity(intent)
         finish()
-        setContentView(R.layout.activity_main_cliente)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
